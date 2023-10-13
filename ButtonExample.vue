c0ff347da8c4660b0a26ebd833196c0692800f5d
jhood4 <44584471+jhood4@users.noreply.github.com>
2023-10-13 16:28:37 -0400

    Create ButtonExample.vue

---
+++ ButtonExample.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    I am a Button
+  </div>
+  <Button :label="data" severity="success" />
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  data: String,
+});
+</script>
