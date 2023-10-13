51ed5cf4e1f1b6efada6266f63114cd61e17635c
jhood4 <44584471+jhood4@users.noreply.github.com>
2023-10-13 16:27:55 -0400

    Create BadgeExample.vue

---
+++ BadgeExample.vue
@@ -0,0 +1,11 @@
+<template>
+  <span> I am a Badge </span>
+  <Badge :value="data" severity="danger" />
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  data: String,
+});
+</script>
